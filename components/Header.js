80f10f602888d731f75703e895c7c94721bdb6f6
Rodrigo Llanos <rodrigo.llanos@zerintia.com>
2020-10-11 13:21:39 +0200

    [FEAT] Header styles done

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,21 @@
-import Home from '../pages';
 import styles from './Header.module.scss';
 import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <header className={styles.header}>
+    <header id='home' className={styles.header}>
       <Navbar />
       <section className={styles.headerContainer}>
         <h1>I'm Rodrigo</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+          enim ad minim veniam.
+        </p>
+        <section className={styles.btnContainer}>
+          <a href='#about'>
+            <i className="fas fa-chevron-circle-down"></i>
+          </a>
+        </section>
       </section>
     </header>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/index.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
